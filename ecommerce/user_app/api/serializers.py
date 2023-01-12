d55ca5dd992a159a8d23b3e009be31bdfb9f31d5
Ragul <rahul.kachiappan@gmail.com>
2023-01-12 14:50:48 +0530

    Adding registration views

--- ecommerce/user_app/api/serializers.py
+++ ecommerce/user_app/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from user_app.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name', 'is_active', 'is_superuser', 'user_type')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password')
+
+# class AddAdminSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('first_name', 'last_name', 'user_name', 'email', 'password')
--- ecommerce/user_app/api/views.py
+++ ecommerce/user_app/api/views.py
@@ -5,21 +5,18 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth import authenticate, login
 from user_app.models import User
-from user_app.api.serializers import UserSerializer
+from user_app.api.serializers import SignUpSerializer
 
 class SignUpView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = SignUpSerializer
 
     def perform_create(self, serializer):
-        user_type = self.request.data.get('user_type')
-        if user_type == 'customer':
-            serializer.save(is_customer=True)
-        elif user_type == 'admin':
-            raise ValidationError("Need to be registered via add admin panel by an existing superuser!")
+        serializer.save(is_active=True)
 
 class AddAdminView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = SignUpSerializer
     permission_classes = (IsAuthenticated, IsAdminUser)
 
     def perform_create(self, serializer):
-        serializer.save(is_admin=True)
+        serializer.save(is_admin=True, is_active=True, is_staff=True, is_superuser=True)
+
